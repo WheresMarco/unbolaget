f7aead7871c5b4955ec4735bd1dcf354310919b3
Marco Hyyryläinen <marco@angrycreative.se>
2020-09-12 17:31:23 +0200

    Sync products to database

--- app/Console/Commands/SyncProducts.php
+++ app/Console/Commands/SyncProducts.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Http\Controllers\SystembolagetController;
+use App\Models\Product;
 
 class SyncProducts extends Command
 {
@@ -48,11 +49,58 @@ class SyncProducts extends Command
      */
     public function handle()
     {
+        $this->info("Starting sync");
+
         $products = $this->systembolaget->getProducts();
 
-        // TODO Only get the products with Type = Beer
+        $this->info("Data fetched");
 
-        // TODO Create or update based on ProductNumber
-        return $products;
+        // TODO Add error handling.
+
+        // TODO Only get the products with Type = Beer
+        if ($products) {
+            foreach ($products as $product) {
+                $this->info("Adding/updating {$product['ProductId']}");
+
+                Product::updateOrCreate(
+                    [
+                        'product_id' => $product['ProductId'],
+                    ],
+                    [
+                        'product_name_bold'  => $product['ProductNameBold'] ?? '',
+                        'product_name_thin'  => $product['ProductNameThin'] ?? '',
+                        'product_nr'  => $product['ProductNumberShort'] ?? '',
+                        'bottle_text'  => $product['BottleTextShort'] ?? '',
+                        'is_organic'  => $product['IsOrganic'] ?? false,
+                        'is_ethical'  => $product['IsEthical'] ?? false,
+                        'is_web_launch'  => $product['IsWebLaunch'] ?? false,
+                        'sell_start_date'  => $product['SellStartDate'] ?? '',
+                        'is_completely_out_of_stock' => $product['IsCompletelyOutOfStock'] ?? false,
+                        'is_temporary_out_of_stock' => $product['IsTemporaryOutOfStock'] ?? false,
+                        'alcohol_percentage'  => $product['AlcoholPercentage'] ?? 0.0,
+                        'volume'  => $product['Volume'] ?? 0.0,
+                        'price'  => $product['Price'] ?? 0.0,
+                        'country'  => $product['Country'] ?? '',
+                        'origin_level_1'  => $product['OriginLevel1'] ?? '',
+                        'origin_level_2'  => $product['OriginLevel2'] ?? '',
+                        'vintage'  => $product['Vintage'] ?? 0,
+                        'sub_category'  => $product['SubCategory'] ?? '',
+                        'type'  => $product['Type'] ?? '',
+                        'style'  => $product['Style'] ?? '',
+                        'beverage_description'  => $product['BeverageDescriptionShort'] ?? '',
+                        'assortment_text'  => $product['AssortmentText'] ?? '',
+                        'usage'  => $product['Usage'] ?? '',
+                        'taste'  => $product['Taste'] ?? '',
+                        'assortment'  => $product['Assortment'] ?? '',
+                        'is_news'  => $product['IsNews'] ?? false,
+                    ]
+                );
+
+                // TODO Add product to category.
+                // TODO Add product to producer.
+            }
+        }
+
+        $this->info('Done');
     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,6 +9,36 @@ class Product extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'product_id',
+        'product_name_bold',
+        'product_name_thin',
+        'product_nr',
+        'bottle_text',
+        'is_organic',
+        'is_ethical',
+        'is_web_launch',
+        'sell_start_date',
+        'is_completely_out_of_stock',
+        'is_temporary_out_of_stock',
+        'alcohol_percentage',
+        'volume',
+        'price',
+        'country',
+        'origin_level_1',
+        'origin_level_2',
+        'vintage',
+        'sub_category',
+        'type',
+        'style',
+        'assortment_text',
+        'beverage_description',
+        'usage',
+        'taste',
+        'assortment',
+        'is_news',
+    ];
+
     public function category()
     {
         return $this->belongsTo('App\Models\Category');
